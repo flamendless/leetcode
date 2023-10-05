42276d0867b5d626d475fe2546167ffeb169fe7f
flamendless <flamendless8@gmail.com>
2023-10-05 14:56:25 +0800

    Time: 42 ms (30.18%), Space: 16.1 MB (88.52%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:   
+        if n < 0:
+            x = 1/x
+            n = -n
+        
+        res: float = 1
+        while n:
+            if n % 2:
+                res *= x
+            x *= x
+            n //= 2
+            
+        return res
