96fdf812d5f00096246326b64397f50a3750c946
flamendless <flamendless8@gmail.com>
2023-10-24 11:38:19 +0800

    Time: 12 ms (54.85%), Space: 4.4 MB (49.30%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.go
@@ -0,0 +1,17 @@
+func isPalindrome(x int) bool {
+    var s string = strconv.Itoa(x)
+    
+    var l int = 0
+    var r int = len(s) - 1
+    
+    for l < len(s) && l < r {
+        if s[l] != s[r] {
+            return false
+        }
+        
+        l += 1
+        r -= 1
+    }
+    
+    return true
+}
