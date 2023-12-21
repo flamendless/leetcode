597067dc430ccd1b425e0f102562ec1ff38fe8ac
flamendless <flamendless8@gmail.com>
2023-12-21 11:46:21 +0800

    Time: 159 ms (32.00%), Space: 22 MB (8.00%) - LeetHub

---
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.go
@@ -0,0 +1,16 @@
+func maxWidthOfVerticalArea(points [][]int) int {
+    sort.Slice(points, func(i int, j int) bool {
+        return points[i][0] < points[j][0]
+    })
+    
+    res := 0
+    
+    for i := 1; i < len(points); i++ {
+        diff := points[i][0] - points[i-1][0]
+        if diff > res {
+            res = diff
+        }
+    }
+    
+    return res
+}
