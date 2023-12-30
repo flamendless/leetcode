495d2c623cf92bcbd5459da121b4765fd5038adf
flamendless <flamendless8@gmail.com>
2023-12-31 07:17:04 +0800

    Time: 19 ms (44.44%), Space: 5.2 MB (11.11%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.go
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.go
@@ -1,13 +1,26 @@
 func makeEqual(words []string) bool {
+    ll := len(words)
     c := map[string]int{}
+    
+    greedy_ret := false
     
     for _, word := range words {
         for _, ch := range word {
-            c[string(ch)]++
+            sch := string(ch)
+            c[sch]++
+            v, _ := c[sch]
+            if v % ll != 0 {
+                greedy_ret = true
+            } else {
+                greedy_ret = false
+            }
         }
     }
     
-    ll := len(words)
+    if greedy_ret {
+        return false
+    }
+    
     for _, v := range c {
         if v % ll != 0 {
             return false
